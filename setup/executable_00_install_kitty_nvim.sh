abd9e3dfb9d70757ba7c73e046e882bef1338572
Kevin Pham <kevin.pham@amtek.com.au>
2020-03-11 17:03:13 +1100

    Updated to ensure kitty is symlinked properly after install

--- setup/executable_00_install_kitty_nvim.sh
+++ setup/executable_00_install_kitty_nvim.sh
@@ -11,6 +11,7 @@
 
 	# Place the kitty.desktop file somewhere it can be found by the OS
 	cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
+	sudo ln -sf ~/.local/kitty.app/bin/kitty ~/bin/kitty
 
 	# Update the path to the kitty icon in the kitty.desktop file
 	sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
