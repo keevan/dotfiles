9c96dbd9089820ae71885bc7b60dae17ce131e7d
Kevin Pham <kevin.pham@amtek.com.au>
2020-03-21 20:20:12 +1100

    Updated vimrc and aliases with some path changes and aliases, and added a setup script for composer and pretissimo

---
+++ setup/executable_20_install_composer.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Install Composer
+
+EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+
+mv composer.phar ~/bin/composer
+source ~/.exports
+
+# Requires php-curl and the extension enabled
+echo "Remember to enable the php-curl extension and install it if not already"
+
+echo "Then run the following:"
+echo "composer global require hirak/prestissimo"
+
+exit $RESULT
