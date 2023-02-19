6b0b111c51b5f76e7546c731b2c83ccbefc2eaf9
Kevin Pham <keevan.pham@gmail.com>
2023-02-19 20:12:53 +1000

    Update setup/40_install_go.sh

--- setup/executable_40_install_go.sh
+++ setup/executable_40_install_go.sh
@@ -1,24 +1,25 @@
 #!/bin/env bash
 
 # Uses https://github.com/keevan/go-installer (forked copy to prevent unknown changes)
-# Doesn't download the script ~ runs the script directly 
+# Doesn't download the script ~ runs the script directly
 cd $HOME
 mkdir -p tmp
 cd tmp
 
 if [[ ! -f go.sh ]]; then
-  wget https://raw.githubusercontent.com/keevan/go-installer/master/go.sh 
+  wget https://raw.githubusercontent.com/keevan/go-installer/master/go.sh
 fi
 
-export GOROOT=/usr/local/go      # install path
+export GOROOT=$HOME/bin/go      # install path
 export GOPATH=$HOME/projects/go  # workspace
 
 # Uninstall golang first
-sudo rm -r /usr/local/go
+sudo rm -r $GOROOT
 
 # Run it
-chmod +x go.sh 
-sudo -E ./go.sh
+chmod +x go.sh
+bash -E ./go.sh
 
 # Reload
 source $HOME/.bashrc
+source $HOME/.exports
