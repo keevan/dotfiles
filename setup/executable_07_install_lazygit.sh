d70159f549d57b9beb6f9e9c732bbb4b4108e83e
Kevin Pham <keevan.pham@gmail.com>
2023-04-21 21:18:22 +1000

    Add setup/07_install_lazygit.sh

---
+++ setup/executable_07_install_lazygit.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+cd /tmp
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+# Clean up after yourself!
+rm lazygit
+rm lazygit.tar.gz
