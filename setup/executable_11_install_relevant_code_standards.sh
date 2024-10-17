202f70a54bbd208a79119d6d2fb1c4f09f5fe8e7
Kevin Pham <keevan.pham@gmail.com>
2024-10-17 15:28:46 +1100

    Change attributes of setup/11_install_relevant_code_standards.sh

--- setup/executable_11_install_relevant_code_standards.sh
+++ setup/executable_11_install_relevant_code_standards.sh
@@ -15,8 +15,9 @@ git clone git@github.com:totara/code-sniffer.git # Totara
 
 # Set the standards on the phpcs (nvim has it's own phpcs path)
 # --
-phpcs --config-set installed_paths "$WORK_DIR/moodle-local_codechecker,$WORK_DIR/code-sniffer/src/Standards,$WORK_DIR/coder/coder_sniffer,$WORK_DIR/PHPCSExtra"
-phpcbf --config-set installed_paths "$WORK_DIR/moodle-local_codechecker,$WORK_DIR/code-sniffer/src/Standards,$WORK_DIR/coder/coder_sniffer,$WORK_DIR/PHPCSExtra"
+installpaths="$WORK_DIR/moodle-local_codechecker/vendor/squizlabs/php_codesniffer/src/Standards/Squiz,$WORK_DIR/moodle-local_codechecker,$WORK_DIR/moodle-local_codechecker/vendor/moodlehq/moodle-cs,$WORK_DIR/moodle-local_codechecker/vendor/phpcompatibility/php-compatibility/PHPCompatibility,$WORK_DIR/code-sniffer/src/Standards,$WORK_DIR/coder/coder_sniffer,$WORK_DIR/PHPCSExtra"
+phpcs --config-set installed_paths "$installpaths"
+phpcbf --config-set installed_paths "$installpaths"
 
 # Set the default (for use outside of nvim, and when phpcs.xml doesn't exist or when you need to specify the standard)
 # --
